677a66d9a1abf8fdaea44b2aff914385926caefb
DayveZZ <abhijitdebofficial@gmail.com>
2025-09-29 13:15:26 +0530

    commit

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -6,13 +6,28 @@ const Todo = () => {
   };
   return (
     <>
+      <h1 className="headTitle">ToDo App Using ContextAPI</h1>
       <form onSubmit={handleSubmit}>
-        <input type="text" />
+        <input type="text" placeholder="Enter your Todo" />
         <button type="submit">ADD</button>
       </form>
 
       <div className="todo">
-        <p>Hello</p>
+        <p>
+          Hello Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius
+          quia animi aspernatur quae aliquam quisquam alias dignissimos
+          voluptatum deserunt et quidem quas qui reiciendis, laudantium eum
+          dolorem commodi dolorum dolor ducimus pariatur quod fugit placeat
+          perspiciatis. Optio, molestias similique nostrum tempora consectetur
+          quibusdam obcaecati cum accusamus voluptatem nesciunt eum consequatur
+          corrupti ipsum facere at. Eligendi aspernatur nobis itaque explicabo
+          quod illo recusandae repellat nihil veritatis laudantium, modi
+          exercitationem odio fuga qui autem corrupti molestiae. Numquam cum
+          dignissimos ipsum placeat voluptatum exercitationem, perferendis quod
+          nihil blanditiis dicta, commodi voluptas natus laudantium deserunt
+          neque? Commodi sit minima impedit necessitatibus, deserunt
+          consequuntur adipisci.
+        </p>
         <button>Delete</button>
         <button>Edit</button>
       </div>
