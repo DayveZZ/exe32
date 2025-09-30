7c3687582b7d54c229d79372f852bef3216105ac
DayveZZ <abhijitdebofficial@gmail.com>
2025-09-30 15:00:41 +0530

    commit

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,36 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Todo = () => {
+  const [tasks, setTasks] = useState("");
+  const [allTask, setAllTask] = useState([]);
+
   const handleSubmit = (e) => {
-    e.preventdefault();
+    e.preventDefault();
+    setAllTask([...allTask, tasks]);
+    console.log(allTask);
+    setTasks("");
+  };
+
+  const handleDelete = (index) => {
+    setAllTask(allTask.filter((_, e) => e !== index));
   };
   return (
     <>
       <h1 className="headTitle">ToDo App Using ContextAPI</h1>
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Enter your Todo" />
+        <input
+          type="text"
+          placeholder="Enter your Todo"
+          value={tasks}
+          onChange={(e) => {
+            setTasks(e.target.value);
+            // console.log(tasks);
+          }}
+        />
         <button type="submit">ADD</button>
       </form>
 
-      <div className="todo">
-        <p>
-          Hello Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius
-          quia animi aspernatur quae aliquam quisquam alias dignissimos
-          voluptatum deserunt et quidem quas qui reiciendis, laudantium eum
-          dolorem commodi dolorum dolor ducimus pariatur quod fugit placeat
-          perspiciatis. Optio, molestias similique nostrum tempora consectetur
-          quibusdam obcaecati cum accusamus voluptatem nesciunt eum consequatur
-          corrupti ipsum facere at. Eligendi aspernatur nobis itaque explicabo
-          quod illo recusandae repellat nihil veritatis laudantium, modi
-          exercitationem odio fuga qui autem corrupti molestiae. Numquam cum
-          dignissimos ipsum placeat voluptatum exercitationem, perferendis quod
-          nihil blanditiis dicta, commodi voluptas natus laudantium deserunt
-          neque? Commodi sit minima impedit necessitatibus, deserunt
-          consequuntur adipisci.
-        </p>
-        <button>Delete</button>
-        <button>Edit</button>
-      </div>
+      {allTask.map((task, index) => (
+        <div className="todo" key={index}>
+          <p>{task}</p>
+          <button onClick={() => handleDelete(index)}>Delete</button>
+          <button>Edit</button>
+        </div>
+      ))}
     </>
   );
 };
